fc287d1dbfc897c14a9603f6c095f1820029ba10
spencer <tealflag@gmail.com>
2021-11-04 23:15:02 -0600

    completed challenge 3 with bonus objectives and debug log

---
+++ New Unity ProjectC/Assets/Challenge 3/Scripts/RepeatBackgroundX.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepeatBackgroundX : MonoBehaviour
+{
+    private Vector3 startPos;
+    private float repeatLimit = 55;
+
+    private void Start()
+    {
+        startPos = transform.position; // Establish the default starting position 
+    }
+
+    private void Update()
+    {
+        // If background moves left by its repeat width, move it back to start position
+        if (Mathf.Abs(startPos.x - transform.position.x) > repeatLimit)
+        {
+            transform.position = startPos;
+        }
+    }
+
+ 
+}
+
+
